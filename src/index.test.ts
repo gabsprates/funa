cec860392020c1f64f135495aa86658f3da5fe5a
Gabriel Prates <gabsprates@gmail.com>
2022-10-12 14:20:23 +0000

    chore: setup testing-library

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,19 @@
+import { screen } from "@testing-library/dom";
+
 import { sum } from ".";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("button should not be disabled", () => {
+  document.body.innerHTML = `<button>click!</button>`;
+
+  expect(screen.getByRole("button")).not.toBeDisabled();
+});
+
+test("button should be disabled", () => {
+  document.body.innerHTML = `<button disabled>click!</button>`;
+
+  expect(screen.getByRole("button")).toBeDisabled();
 });
